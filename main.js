3c65692630d2d2e8064df0a84ba1ebda6731954f
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2015-04-22 19:31:32 -0300

    Adding unity build.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function main() {
   var u = new UnityObject2();
-  u.initPlugin(jQuery("#unityPlayer")[0], "Example.unity3d");
+  u.initPlugin(jQuery("#unityPlayer")[0], "Build.unity3d");
 }
 
 $(document).ready(main);
